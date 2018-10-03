3c8a7cd013dcc86f680c046fba14ffa966288e1b
Connor Tumbleson <connor@sourcetoad.com>
2018-10-03 16:14:31 -0400

    fix: update network script for MariaDB10.2

--- docker/data-source-services/network-creation.sh
+++ docker/data-source-services/network-creation.sh
@@ -1,6 +1,6 @@
 echo "Creating container networks:\n"
 docker network create nginx-proxy
-docker network create st-mariadb-101
+docker network create st-mariadb-102
 docker network create st-mysql-56
 docker network create st-postgres-95
 docker network create st-redis-32
