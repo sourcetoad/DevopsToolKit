f31721e33b9fda16421ddd2ed24a7849ac383c70
Chavez Ellis <chavez.ellis@sourcetoad.com>
2022-04-13 17:11:16 -0400

    JEST-106 fix: branch was out of time. Fixed issue

--- scripts/db_migrate.sh
+++ scripts/db_migrate.sh
@@ -9,7 +9,6 @@ DB_HOST='127.0.0.1'
 DB_USER='root'
 DB_PASS='root'
 DB_EXPORT_FLAGS='--routines --quick --hex-blob --single-transaction'
-DB_IMPORT_FLAGS=''
 
 # Determine DB
 echo "Welcome to the SDIT DB Migrator (mysql/postgres) $NEWLINE"
@@ -22,14 +21,8 @@ read -rp "Enter (TO) database name: " DB_TO_NAME
 # Given the output of "docker ps" and a port, can find a matching line of:
 # 6eabb950ac3f   mariadb:10.2  "docker-entrypoint.s…" 2 weeks ago Up 30 hours 0.0.0.0:33102->3306/tcp sourcetoad_mariadb102
 # and extract "sourcetoad_mariadb102"
-<<<<<<< HEAD
 CONTAINER_NAME_FROM=$(docker ps | grep "$DB_PORT" | rev | cut -d" " -f1 | rev)
 CONTAINER_NAME_TO=$(docker ps | grep "$DB_TO_PORT" | rev | cut -d" " -f1 | rev)
-=======
-CONTAINER_NAME_FROM=$(docker ps | grep $DB_PORT | rev | cut -d" " -f1 | rev)
-CONTAINER_NAME_TO=$(docker ps | grep $DB_TO_PORT | rev | cut -d" " -f1 | rev)
-
->>>>>>> 96402b5b32565b6d9b4533c92d2607dce1db81b1
 
 case $DB_MODE in
     mysql|maria|mariadb)
@@ -47,10 +40,7 @@ case $DB_MODE in
         fi
 
         echo -n "Copying...! $NEWLINE"
-<<<<<<< HEAD
         # shellcheck disable=SC2086
-=======
->>>>>>> 96402b5b32565b6d9b4533c92d2607dce1db81b1
         docker exec -i "$CONTAINER_NAME_FROM" mysqldump $DB_EXPORT_FLAGS --user="${DB_USER}" --password="${DB_PASS}" --port="${DB_PORT}" --databases "${DB_NAME}" | docker exec -i "$CONTAINER_NAME_TO" mysql --user="${DB_USER}" --password="${DB_PASS}" --port="${DB_TO_PORT}" "${DB_TO_NAME}"
         echo -n "Copied...! $NEWLINE"
     ;;
